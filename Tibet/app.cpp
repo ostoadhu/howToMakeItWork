8cfee950619075862f06bcf0de5d4aa2af8de7c9
Adam <adam.ostorhazi@bbraun.com>
2025-09-30 15:33:30 +0200

    Tibet_InitialCommit

---
+++ Tibet/app.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+//g++ -Wall *.cpp -o tibet
+
+// number of arguments : argc
+// argv is an array of pointers to char types
+int main(int argc, char *argv[])
+{
+    std::cout << "Tibet Application" << std::endl;
+
+    
+
+
+
+
+
+
+
+    return 0;
+}
