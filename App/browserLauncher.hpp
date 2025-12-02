dd60544769676d3209214370cfea835040fcd9d5
Adam <adam.ostorhazi@bbraun.com>
2025-12-02 01:08:47 +0100

    RenamingAndNormalizing

--- App/browserLauncher.hpp
+++ App/browserLauncher.hpp
@@ -2,14 +2,10 @@
 
 class BrowserLauncher {
 private:
-    // Need constructor BC we have states (member variables)
     int result = -1;
     std::string command = "NULL";
 
 public:
-    // Constructor
-    BrowserLauncher() : result{-1}, command{"NULL"} {};
-
     // public methods
     void launch(const std::string& url) 
     {
--- App/main.cpp
+++ App/main.cpp
@@ -4,16 +4,19 @@
 // user defined headers to see protypes and types
 #include "browserLauncher.hpp"
 
-//g++ -Wall *.cpp -o tibet
+//g++ -Wall *.cpp -o app
+// ./app
 
 // number of arguments : argc
 // argv is an array of pointers to char types
 int main(int argc, char *argv[])
 {
-    std::cout << "Tibet Application" << std::endl;
+    std::cout << "The Application" << std::endl;
 
+    // initialize launcher object
     BrowserLauncher launcher;
 
+    // Open a particular webpage
     launcher.launch("https://google.com");
 
 
